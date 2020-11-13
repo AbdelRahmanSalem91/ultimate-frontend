506d95a6f37683cb34205e61d4f0fea31d399b43
AbdelRahmanSalem91 <abdoahmed424@gmail.com>
2020-11-13 14:35:30 +0200

    Lesson 14

--- dist/js/main.js
+++ dist/js/main.js
@@ -4,5 +4,11 @@ $(function(){
         $('.content-area, .sidebar').toggleClass('no-sidebar');
     })
 })
+
+// Toggle Submenu
+$(".toggle-submenu").on('click', function(){
+    $(this).find(".fa-angle-right").toggleClass("down");
+    $(this).next(".child-links").slideToggle();
+})
 const myMap = new Map();
 const mySet = new Set();
--- stage/js/main.js
+++ stage/js/main.js
@@ -4,3 +4,9 @@ $(function(){
         $('.content-area, .sidebar').toggleClass('no-sidebar');
     })
 })
+
+// Toggle Submenu
+$(".toggle-submenu").on('click', function(){
+    $(this).find(".fa-angle-right").toggleClass("down");
+    $(this).next(".child-links").slideToggle();
+})
