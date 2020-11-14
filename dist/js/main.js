4e396c66451da74723d5a3f2d92456e69041d750
AbdelRahmanSalem91 <abdoahmed424@gmail.com>
2020-11-14 15:24:34 +0200

    Lesson 18

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,12 +3,46 @@ $(function(){
     $('.toggle-sidebar').on('click', function(){
         $('.content-area, .sidebar').toggleClass('no-sidebar');
     })
-})
+});
 
 // Toggle Submenu
 $(".toggle-submenu").on('click', function(){
     $(this).find(".fa-angle-right").toggleClass("down");
     $(this).next(".child-links").slideToggle();
-})
+});
+
+// Open / Close Fullscreen
+$('.toggle-fullscreen').on('click', function(){
+    $(this).toggleClass("full-screen");
+    if($(this).hasClass('full-screen')){
+        openFullscreen()
+    }else{
+        closeFullscreen()
+    }
+});
+
+var elem = document.documentElement;
+
+/* View in fullscreen */
+function openFullscreen() {
+    if (elem.requestFullscreen) {
+            elem.requestFullscreen();
+        } else if (elem.webkitRequestFullscreen) { /* Safari */
+            elem.webkitRequestFullscreen();
+        } else if (elem.msRequestFullscreen) { /* IE11 */
+            elem.msRequestFullscreen();
+    }
+}
+
+/* Close fullscreen */
+function closeFullscreen() {
+if (document.exitFullscreen) {
+    document.exitFullscreen();
+} else if (document.webkitExitFullscreen) { /* Safari */
+    document.webkitExitFullscreen();
+} else if (document.msExitFullscreen) { /* IE11 */
+    document.msExitFullscreen();
+}
+        }
 const myMap = new Map();
 const mySet = new Set();
